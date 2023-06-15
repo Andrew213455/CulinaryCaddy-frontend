146fc04ee3e1cd49ead7f52d8bdb281de7bcd434
Nick Grundberg <nick.grundberg@gmail.com>
2023-06-15 15:28:57 -0400

    fix overwrite error

--- src/components/TimerCard.tsx
+++ src/components/TimerCard.tsx
@@ -1,42 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Step } from "../models/Directions";
 import "./TimerCard.css";
-
 interface Props {
   step: Step;
 }
-
 const TimerCard = ({ step }: Props) => {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [trigger, setTrigger] = useState(false);
+  let interval: NodeJS.Timer | undefined;
   const getTime = () => {
     // const time = Date.parse(deadline) - Date.now();
   };
-
+  useEffect(() => {
+    if (trigger) {
+      interval = setInterval(() => {
+        setSeconds((prev) => prev + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [trigger]);
+  if (seconds > 59) {
+    setSeconds(0);
+    setMinutes(minutes + 1);
+  }
   if (step?.length !== undefined) {
     console.log(step?.length.number);
   }
-
   return (
     <div className="TimerCard">
-      <table>
-        <thead>
-          <tr>
-            <td>
-              <span>Show Timer</span>
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>blah</td>
-          </tr>
-        </tbody>
-      </table>
-      {step?.length !== undefined ? <p>{step.length.number}</p> : <p>derp</p>}
+      <button className="start" onClick={() => setTrigger(true)}>
+        set timer
+      </button>
+      <div>
+        <p>
+          {" "}
+          {hours} : {minutes} : {seconds}
+        </p>
+      </div>
+      {/* {step?.length !== undefined ? <p>{step.length.number}</p> : <p>derp</p>} */}
     </div>
   );
 };
-
 export default TimerCard;
